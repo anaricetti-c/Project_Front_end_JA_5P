e6286268b6546f57138c6aa17ac66dd190ad6014
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-06-05 13:06:37 -0300

    feat: update access deined style

--- src/Pages/AccessDenied/index.jsx
+++ src/Pages/AccessDenied/index.jsx
@@ -1,19 +1,18 @@
-
-import StyledLink from '../../Components/StyledLink';
-import './style.css'
+import StyledLink from "../../Components/StyledLink";
+import styles from "./style.module.css";
 
 export default function AccessDenied() {
   return (
-    <div className="access-container">
-      <div className="access-card">
-        <div className="access-header">Acesso negado</div>
-        <div className="access-body">
-            {/* <BsSignStopFill size={90} color="red" className="access-icon" /> */}
-            <p className="access-message">
-                Você não tem permissão para acessar essa página ou o token está expirado. Por favor faça o seu
-                <StyledLink to="/" color='#dc3545' text={" login "}></StyledLink>.
-            </p>
-            
+    <div className={styles.accessContainer}>
+      <div className={styles.accessCard}>
+        <div className={styles.accessHeader}>Acesso negado</div>
+        <div className={styles.accessBody}>
+          {/* <BsSignStopFill size={90} color="red" className="access-icon" /> */}
+          <p className={styles.accessMessage}>
+            Você não tem permissão para acessar essa página ou o token está
+            expirado. Por favor faça o seu
+            <StyledLink to="/" color="#dc3545" text={" login "}></StyledLink>.
+          </p>
         </div>
       </div>
     </div>

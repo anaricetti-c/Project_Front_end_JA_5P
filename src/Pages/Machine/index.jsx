2b3e5fdc042b4acc95a4ba13e1b91f641a01b72e
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-06-09 20:35:54 -0300

    feat: add filter and delete to machine

--- src/Pages/Machine/index.jsx
+++ src/Pages/Machine/index.jsx
@@ -1,12 +1,14 @@
 import qs from "qs";
+import { toast } from "react-toastify";
 import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 import styles from "./style.module.css";
 
 import { api } from "../../services/api";
 import { getHeaders } from "../../services/headers";
 import PaginatedTable from "../../components/PaginatedTable";
+import DeleteModal from "../../components/DeleteModal";
 
 export default function Machine() {
   const [loading, setLoading] = useState(false);
@@ -14,6 +16,13 @@ export default function Machine() {
   const [search, setSearch] = useSearchParams();
   const [total, setTotal] = useState(0);
   const [selectedFilter, setSelectedFilter] = useState("");
+  const [pageSize, setPageSize] = useState(15);
+  const [machineToDelete, setMachineToDelete] = useState(null);
+  const navigate = useNavigate();
+
+  function handleDeleteClick(machine) {
+    setMachineToDelete(machine);
+  }
 
   useEffect(() => {
     const page = Number(search.get("page"));
@@ -25,6 +34,20 @@ export default function Machine() {
   async function fetchMachines(page) {
     setLoading(true);
     try {
+      const field = search.get("field");
+      const value = search.get("value");
+
+      const requestParams = {
+        // associations: ["customer", "created_by"],
+        limit: pageSize,
+        page: page,
+      };
+
+      if (field && value) {
+        requestParams.field = `machine.${field}`;
+        requestParams.value = value;
+      }
+
       const machinesResponse = await api.get(`/machine/all`, {
         headers: getHeaders(),
         params: {
@@ -32,12 +55,12 @@ export default function Machine() {
           page: page,
           limit: 10,
         },
-        paramsSerializer: (params) =>
-          qs.stringify(params, { arrayFormat: "repeat" }),
+        // paramsSerializer: (params) =>
+        //   qs.stringify(params, { arrayFormat: "repeat" }),
       });
 
-      setTotal(machinesResponse.data.metadata.total);
       console.log(machinesResponse.data);
+      setTotal(machinesResponse.data.metadata.total);
       setMachines(machinesResponse.data.data);
     } catch (error) {
       console.error("Error fetching machines:", error);
@@ -46,13 +69,41 @@ export default function Machine() {
     }
   }
 
+  async function confirmDeleteMachine() {
+    try {
+      await api.delete(`/machine/delete/${machineToDelete.id}`, {
+        headers: getHeaders(),
+      });
+
+      const currentPage = Number(search.get("page")) || 1;
+      const newTotal = total - 1;
+
+      const maxPage = Math.ceil(newTotal / pageSize);
+      const newPage = currentPage > maxPage ? maxPage : currentPage;
+
+      setSearch((prev) => {
+        const newParams = new URLSearchParams(prev);
+        newParams.set("page", newPage);
+        newParams.set("refresh", Date.now().toString());
+        return newParams;
+      });
+
+      setMachineToDelete(null);
+    } catch (error) {
+      console.error("Erro ao deletar maquina:", error);
+      toast.error("Erro ao deletar maquina.");
+    }
+  }
+
   function getMachines() {
     if (Array.isArray(machines) && machines.length > 0) {
       return machines.map((m, index) => {
         const status =
-          m.status === 'Under Maintenance' ? "under-maintenance" : 
-          m.status === 'Out of Service' ? 'out-of-service' :
-          m.status.toLowerCase();
+          m.status === "Under Maintenance"
+            ? "under-maintenance"
+            : m.status === "Out of Service"
+            ? "out-of-service"
+            : m.status.toLowerCase();
         return (
           <tr key={m.id}>
             <td>{m.name}</td>
@@ -61,15 +112,24 @@ export default function Machine() {
               <span className={`status status-${status}`}>{m.status}</span>
             </td>
             <td>
-              <img src="delete.png" alt="Deletar" className="icon" />
+              <img
+                onClick={() => navigate(`/machine/${m.id}`)}
+                src="details.png"
+                alt="Detalhes"
+                className="icon"
+              />
+            </td>
+            <td>
+              <img
+                onClick={() => handleDeleteClick(m)}
+                src="delete.png"
+                alt="Deletar"
+                className="icon"
+              />
             </td>
           </tr>
         );
       });
-    
-
-      
-
     } else {
       return (
         <tr>
@@ -90,7 +150,13 @@ export default function Machine() {
         filters={"machine"}
         selectedFilter={selectedFilter}
         setSelectedFilter={setSelectedFilter}
-        columns={["Código", "Nome", "Status", ""]}
+        columns={["Código", "Nome", "Status", "", ""]}
+      />
+      <DeleteModal
+        confirmDeleteEntity={confirmDeleteMachine}
+        setEntityToDelete={setMachineToDelete}
+        entityToDelete={machineToDelete}
+        entityName={"a máquina"}
       />
     </main>
   );

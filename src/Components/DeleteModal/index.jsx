850ee48a3b5c7e9ac489402dedb8273d1c451706
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-06-09 19:27:56 -0300

    feat: create DeleteModal

---
+++ src/Components/DeleteModal/index.jsx
@@ -0,0 +1,36 @@
+import styles from "./style.module.css";
+
+export default function DeleteModal({
+  confirmDeleteEntity,
+  setEntityToDelete,
+  entityToDelete,
+  entityName,
+}) {
+  if (!entityToDelete) return null;
+
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
+        <h3>Confirmar exclusão</h3>
+        <p>
+          Tem certeza que deseja excluir o {entityName}{" "}
+          <strong>{entityToDelete.name}</strong>?
+        </p>
+        <div className={styles.modalActions}>
+          <button
+            onClick={() => confirmDeleteEntity()}
+            className={styles.confirmButton}
+          >
+            Confirmar
+          </button>
+          <button
+            onClick={() => setEntityToDelete(null)}
+            className={styles.cancelButton}
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}

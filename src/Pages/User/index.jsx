c02e2a15a51c273016a3a4f4e187174723dea4b3
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-06-09 22:11:17 -0300

    feat: create user page

--- src/Pages/User/index.jsx
+++ src/Pages/User/index.jsx
@@ -1,55 +1,111 @@
-import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useState, useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 import styles from "./style.module.css";
+
 import { api } from "../../services/api";
 import { getHeaders } from "../../services/headers";
+import CustomPagination from "../../components/CustomPagination";
+import CustomSelect from "../../components/CustomSelect";
+import PaginatedCards from "../../components/PaginatedCards";
 
 export default function User() {
+  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useSearchParams();
+  const [total, setTotal] = useState(0);
+  const [selectedFilter, setSelectedFilter] = useState("");
+
+  // const [userToDelete, setUserToDelete] = useState(null);
+  // const navigate = useNavigate();
+
+  // function handleDeleteClick(mold) {
+  //   setUserToDelete(mold);
+  // }
 
   useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const response = await api.get("/user/all", {
-          headers: getHeaders(),
-        });
-        setUsers(response.data.data || []);
-      } catch (err) {
-        console.error("Erro ao buscar usuários:", err);
-      } finally {
-        setLoading(false);
-      }
+    const page = Number(search.get("page"));
+    if (page) {
+      fetchUsers(page);
     }
+  }, [search]);
 
-    fetchUsers();
-  }, []);
+  async function fetchUsers(page) {
+    setLoading(true);
+    try {
+      const field = search.get("field");
+      const value = search.get("value");
 
-  if (loading) return <p>Carregando usuários...</p>;
+      const requestParams = {
+        // associations: [],
+        limit: 6,
+        page: page,
+      };
 
-  return (
-    <div className={styles.container}>
-      <h1>Usuários</h1>
-      <div className={styles.cardGrid}>
-        {users.map((user) => (
-          <div key={user.id} className={styles.card}>
-            <h2>{user.full_name}</h2>
+      if (field && value) {
+        requestParams.field = `user.${field}`;
+        requestParams.value = value;
+      }
+
+      const usersResponse = await api.get(`/user/all`, {
+        headers: getHeaders(),
+        params: requestParams,
+        // paramsSerializer: (params) =>
+        //   qs.stringify(params, { arrayFormat: "repeat" }),
+      });
+
+      console.log(usersResponse.data);
+      setTotal(usersResponse.data.metadata.total);
+      setUsers(usersResponse.data.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  function getUsers() {
+    if (Array.isArray(users) && users.length > 0) {
+      return users.map((u) => {
+        return (
+          <div key={u.id} className={styles.card}>
+            <h2>{u.full_name}</h2>
             <p>
-              <strong>Email:</strong> {user.email}
+              <strong>Email:</strong> {u.email}
             </p>
             <p>
-              <strong>Matrícula:</strong> {user.registration_number}
+              <strong>Matrícula:</strong> {u.registration_number}
             </p>
             <p>
-              <strong>Cargo:</strong> {user.role}
+              <strong>Cargo:</strong> {u.role}
             </p>
             <p className={styles.createdAt}>
               <small>
-                Criado em: {new Date(user.created_at).toLocaleDateString()}
+                Criado em: {new Date(u.created_at).toLocaleDateString()}
               </small>
             </p>
           </div>
-        ))}
-      </div>
-    </div>
+        );
+      });
+    } else {
+      return <p>Nenhum usuário cadastrado.</p>;
+    }
+  }
+
+  return (
+    <main className={styles.main}>
+      <PaginatedCards
+        title="Usuários"
+        filters={"user"}
+        getCards={getUsers}
+        loading={loading}
+        search={search}
+        selectedFilter={selectedFilter}
+        setSearch={setSearch}
+        setSelectedFilter={setSelectedFilter}
+        total={total}
+      />
+    </main>
   );
 }
